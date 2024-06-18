1892d132b13a8574deedf49520bd5bfa83a01bb0
Brett Jia <dev.bjia56@gmail.com>
2024-06-18 00:17:24 -0400

    build rkmpp

---
+++ builder/scripts.d/1-rkmpp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/nyanmisaka/mpp.git"
+SCRIPT_COMMIT="jellyfin-mpp"
+
+ffbuild_enabled() {
+    [[ $TARGET == linux* ]] && [[ $TARGET == *arm64 ]] && return -
+    return -1
+}
+
+ffbuild_dockerbuild() {
+    git clone "$SCRIPT_REPO" mpp
+    cd mpp
+    git checkout "$SCRIPT_COMMIT"
+
+    mkdir rkmpp_build
+    cmake \
+        -DENABLE_RKMPP=ON \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DENABLE_SHARED=OFF \
+        -DBUILD_TEST=OFF \
+        ..
+
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-rkmpp
+}
+
+ffbuild_unconfigure() {
+    echo --disable-rkmpp
+}
